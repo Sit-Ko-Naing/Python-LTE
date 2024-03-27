22819c91967c0f9bd38373c8ae4294772e1719f0
Sit-Ko-Naing <sitkonaing.esrc@gmail.com>
2024-03-27 17:19:44 +0630

    four

---
+++ day4.py
@@ -0,0 +1,62 @@
+# # name = "Sit"
+# # age = 24
+# # marks = 87.2
+# # town = "Yangon"
+
+# # total = "My name is " + name 
+
+# # print(total)
+
+
+# '''
+# My name is ____ I live in ____ . I'm ___ years old.
+
+# '''
+
+# name = "Kyaw Kyaw"
+# age = 24
+# town = "Yangon"
+
+# print(f"My name is {name}.I live in {town}.I'm {age} years old.")
+
+# -----------------------------------------------------------------------------------
+
+name = "Kyaw Kyaw"
+
+print(name.lower())
+print(name.title())
+print(name.upper())
+print(name.capitalize())
+
+# -----------------------------------------------------------------------------------
+
+txt = "hello, welcome to my python class"
+
+print(len(txt))
+print(txt.index("t"))
+
+# --------------------------------------------------------------------------------------
+
+str_1 = "Hello, welcome to my class"
+
+print(str_1[0])
+print(str_1[-4])
+
+# ---------------------------------------------------------------------------------------
+
+str_1 = "Hello World"
+
+print(str_1[:5])
+print(str_1[6:])
+print(str_1[2:9])
+
+# ------------------------------------------------------------------------------------------
+
+num1 = float(input("enter first num :"))
+num2 = float(input("enter second num :"))
+
+sum = num1 + num2
+
+print(f"Totla value is : {sum}")
+
+# -------------------------------------------------------------------------------------------
---
+++ test.py
@@ -0,0 +1,6 @@
+num1 = float(input("enter first num :"))
+num2 = float(input("enter second num :"))
+
+sum = num1 + num2
+
+print(f"Totla value is : {sum}")
