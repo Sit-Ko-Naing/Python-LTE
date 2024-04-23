d1387e11dc04de8057ef10e89b44899463e69764
Sit-Ko-Naing <sitkonaing.esrc@gmail.com>
2024-04-23 20:25:11 +0630

    tenn

--- day10.py
+++ day10.py
@@ -36,3 +36,20 @@ scale2 = random.randint(0,100)
 print(scale2)
 
 # ------------------------------------------------------------------------
+
+import random       # libs
+
+computer_num = random.randint(1,5) # 1 ----- 10        #cmp number
+user_num = int(input("Enter a number [1,5] : ")) #usr_input_num --23
+
+if user_num <= 5:
+    if computer_num > user_num:
+        print(f"Computer win. cp {computer_num} | usr {user_num}")
+    elif computer_num < user_num:
+        print(f"User win. cp {computer_num} | usr {user_num}") 
+    elif computer_num == user_num:
+        print(f"Draw. cp {computer_num} | usr {user_num} ")  
+    else:
+        print("Something Wrong. Try again.")
+else:
+    print("Sorry, you can input between 1 to 10.")
